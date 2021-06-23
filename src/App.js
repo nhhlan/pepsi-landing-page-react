b4d5cd313ee1894911209ab007f7625db32c51c7
kimehami <59031372+kimehami@users.noreply.github.com>
2021-06-23 14:39:02 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Home } from './components/pages/HomePage/Home';
 import { Products } from './components/pages/Products/Products';
